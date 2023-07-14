763917d75b10ecd8438d61a4c4cc9089168fd20b
Alex Van Kuiken <alexzvk@umich.edu>
2023-07-14 15:09:58 -0400

    updated bot messages

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class MyClient(discord.Client):
             response = requests.get(url, headers=self.headers)
             try:
                 self.current_movie = json.loads(response.text)['results'][0]['id'] # get first movie id from search (too much work to iterate through all search results)
-                self.timer_message = await message.channel.send(f'Movie {json.loads(response.text)["results"][0]["title"]} is valid. Game will end <t:{remaining_time}:R>')
+                self.timer_message = await message.channel.send(f'Movie "{json.loads(response.text)["results"][0]["title"]}" is valid.\nCurrently on turn 1.\nGame will end <t:{remaining_time}:R>')
             except:
                 return await message.channel.send("Sorry, I couldn't find that movie.")
         
@@ -62,7 +62,7 @@ class MyClient(discord.Client):
             if self.timer_message:
                 await self.timer_message.delete()
             self.list_of_movies = movies_actor_is_in
-            self.timer_message = await message.channel.send(f'Actor {actor_name} is valid. Game will end <t:{remaining_time}:R>')
+            self.timer_message = await message.channel.send(f'Actor "{actor_name}" is valid.\nCurrently on turn {self.turns + 1}.\nGame will end <t:{remaining_time}:R>')
         # even numbered turns we check if movie has particular actor
         else:
             movie_code, movie_name = self.verify_movie_has_actor(message_content, self.list_of_movies)
@@ -73,7 +73,7 @@ class MyClient(discord.Client):
             if self.timer_message:
                 await self.timer_message.delete()
             self.current_movie = movie_code
-            self.timer_message = await message.channel.send(f'Movie {movie_name} is valid. Game will end <t:{remaining_time}:R>')
+            self.timer_message = await message.channel.send(f'Movie "{movie_name}" is valid.\nCurrently on turn {self.turns + 1}.\nGame will end <t:{remaining_time}:R>')
 
         # only on a successful turn do we increment the turn and bar the player from making two moves in a row
         self.turns += 1
@@ -101,7 +101,7 @@ class MyClient(discord.Client):
             self.list_of_movies = []
             self.current_movie = -1
             self.previous_player = None
-            return await message.channel.send(f'Game over!\nThis game lasted {total_turns} turns over {time_elapsed_string}.\nThe high score is {self.high_score} turns.')
+            return await message.channel.send(f'Game over!\nThis game lasted {total_turns} turns over {time_elapsed_string}.\nThe high score is {self.high_score} turns.\n@ me with a movie to play again!')
         
     
     def verify_actor_in_movie(self, actor_string, movie_id):
