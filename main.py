179cc43fc5819a24ee2fc53a11f2d893592df3d6
Alex Van Kuiken <alexzvk@umich.edu>
2023-09-19 18:59:41 -0400

    remove random flask

--- main.py
+++ main.py
@@ -265,11 +265,3 @@ class MyClient(discord.Client):
 client = MyClient(intents=intents)
 client.run(os.getenv("BOT_TOKEN"))
 
-
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def index():
-  return "Bot up and running"
-if __name__ == '__main__':
-  app.run(host="0.0.0.0",debug=True,port=8080)
